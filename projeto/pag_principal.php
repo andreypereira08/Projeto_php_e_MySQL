6bf993fe16def88788d9bf04e9aaa289014095da
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2023-02-11 17:37:10 -0300

    melhorias_1

--- projeto/pag_principal.php
+++ projeto/pag_principal.php
@@ -28,30 +28,9 @@
     header("location: index.html");
     exit;
   }
-
+  
+  include "menu_nav.php";
   ?>
-  <nav class="navbar navbar-expand-md  fixed-top maine-menu">
-    <div class="container">
-      <button class="navbar-toggler ml-auto" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse"> <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
-      <style>
-        #menuestranho{
-          color: white;
-        }
-        #menuestranho:hover{
-          color: rgba(255, 255, 255, 0.638);
-        }
-      </style>
-      <div id="my-nav" class="collapse navbar-collapse">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item"><p class="nav-link"><?php echo $_SESSION['email']." | ".$_SESSION['cod_usuario'];?></p></li>
-         <li class="nav-item"> <a class="nav-link" id="menuestranho" href="pag_principal.php">Home</a> </li>
-          <li class="nav-item"> <a class="nav-link" id="menuestranho" href="playlist.php" tabindex="-1" aria-disabled="true">Favoritos</a></li>
-          <li class="nav-item"> <a class="nav-link" id="menuestranho" href="sair.php" tabindex="-1" aria-disabled="true">Sair</a></li>
-         
-        </ul>
-      </div>
-    </div>
-  </nav>
 
   
 
--- projeto/playlistadm.php
+++ projeto/playlistadm.php
@@ -30,43 +30,10 @@
 
 //   echo $_SESSION['email'];
   // include "tema_esc_cla/index.html";
+
+  // sem verificacao de adm
+  include "menu_nav_adm.php";
   ?>
-  <nav class="navbar navbar-expand-md  fixed-top maine-menu">
-    <div class="container">
-      <button class="navbar-toggler ml-auto" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse"> <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
-      <style>
-        #menuestranho{
-          color: white;
-        }
-        #menuestranho:hover{
-          color: rgba(255, 255, 255, 0.638);
-        }
-      </style>
-      <div id="my-nav" class="collapse navbar-collapse">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item"><p class="nav-link"><?php echo $_SESSION['email']." | ".$_SESSION['cod_usuario'];?></p></li>
-         <li class="nav-item"> <a class="nav-link" id="menuestranho" href="index.php">Home</a> </li>
-
-          <li class="nav-item"> <a class="nav-link" id="menuestranho" href="playlistadm.php" aria-disabled="true">Favoritos</a></li>
-          
-          <!-- dropdown menu -->
-          <div class="dropdown nav-item">
-           <a class="nav-link dropdown-toggle" style="color: white; font-weight: bold; text-transform: uppercase;" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administração</a>
-
-           <div class="dropdown-menu" style="background-color: #E90E65;" aria-labelledby="dropdownMenuLink">
-              <a class="nav-link dropdown-item" style="text-transform: uppercase;" href="contas.php" aria-disabled="true">Contas</a>
-              <a class="nav-link dropdown-item" style="text-transform: uppercase;" href="musicas.php" aria-disabled="true">Músicas</a>
-              <a class="nav-link dropdown-item" style="text-transform: uppercase;" href="artistas.php" aria-disabled="true">Artistas</a>
-
-           </div>
-          </div>
-          <!-- Fim Dropdown menu -->
-
-          <li class="nav-item"> <a class="nav-link" id="menuestranho" href="sair.php" tabindex="-1" aria-disabled="true">Sair</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
 
   <div class="container-fluid fh5co-recent-work">
 
