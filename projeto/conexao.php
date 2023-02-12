a601ab84c948c3e025712a67e1f2a8b29123a9b5
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2023-02-12 14:22:31 -0300

    atualiza

--- projeto/conexao.php
+++ projeto/conexao.php
@@ -1,9 +1,14 @@
 <?php
 
-    $servidor="localhost";
-    $usuario="root";
-    $senha="";
-    $db="bdprojeto";
+    // $servidor="localhost";
+    // $usuario="root";
+    // $senha="";
+    // $db="bdprojeto";
+
+    $servidor = "sql309.ezyro.com";
+    $usuario = "ezyro_33212438";
+    $senha = "fe6hvsc";
+    $db = "ezyro_33212438_bdprojeto";
     
     $conexao=mysqli_connect($servidor, $usuario, $senha, $db);
 	$_SESSION['conexao'] = $conexao;
